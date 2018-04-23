c0a7bcf74175e66113eb632d28da3a100db23c78
Anne <anne.derenoncourt@gmail.com>
2018-04-22 21:17:08 -0300

    programmation formulaire contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,7 +9,9 @@
 namespace App\Controller;
 
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends Controller
@@ -18,9 +20,29 @@ class ContactController extends Controller
     /**
      * @Route("/contact", name="contact")
      */
-    public function indexAction()
+    public function indexAction(Request $request, \Swift_Mailer $mailer)
     {
-        return $this->render('contact/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if($form ->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $message= (new \Swift_Message('Contact to ADACVG'))
+                ->setFrom($data['email'])
+                ->setTo('anne.derenoncourt@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'Emails/contact.html.twig',
+                        array('data' => $data)
+                    ),
+                    'text/html'
+                );
+            $mailer->send($message);
+            $this->addFlash('notice', 'Votre message a été envoyé. Merci!');
+            return $this->redirectToRoute('contact');
+        }
+        return $this->render('contact/contact.html.twig', array('form' => $form->createView()));
     }
 
 
